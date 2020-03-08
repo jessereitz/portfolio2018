52efa1716d3eee9ccca611f2b451d914771b48c8
Jesse Reitz <jessereitz1@gmail.com>
2020-03-08 12:32:31 -0500

    Get everything running as expected

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,8 @@
 FROM nginx:1.17.7-alpine
 
+RUN rm /etc/nginx/conf.d/default.conf
 COPY ./data/nginx/app.conf /etc/nginx/conf.d
 COPY ./src /var/www/jessereitz.com
+
+EXPOSE 8080
+CMD ["nginx", "-g", "daemon off;"]
--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,11 @@
 #!/usr/bin/env bash
 
-server_name=port-port-portfolio
 project_id=jreitz-portfolio
 tag=gcr.io/$project_id/portfolio
 
-remote_command='cd portfolio2018 && git pull && docker-compose pull && docker-compose down && docker-compose up -d'
-
-
-echo "Deploying updated portfolio site to server ${server_name}"
 echo "Building image ${tag}"
 
 docker build -t $tag . &&
-docker push $tag &&
+# docker push $tag &&
 
-echo "Image built and pushed to GCR"
-
-gcloud compute ssh \
-    $server_name \
-    --command "${remote_command}"
-
-echo "Deployment done."
+echo "Image built and pushed to GCR. Remember to create a new deployment in Cloud Run"
